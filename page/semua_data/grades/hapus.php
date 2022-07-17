d05789c3756e37b87f4877ab7fb990c66c017410
Nugroho Eko Saputra Batubara <109465924+Nugrohonesb@users.noreply.github.com>
2022-07-17 19:22:16 +0700

    Delete page directory

--- page/semua_data/grades/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "../../../asset/inc/config.php";
-	$urutan = mysqli_real_escape_string($koneksi,$_GET['urutan']);
-	$query = mysqli_query($koneksi,"DELETE FROM tb_grades WHERE urutan='$urutan' ");
-	header('location:../../../index.php?page=grades');
-?>
--- page/setting/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "../../asset/inc/config.php";
-	$id_user = mysqli_real_escape_string($koneksi,$_GET['id_user']);
-	$query = mysqli_query($koneksi,"DELETE FROM tb_user WHERE id_user='$id_user' ");
-	header('location:../../index.php?page=user');
-?>
